926dc42d5fe90bebc0618a1057c471175d25babe
Harrison <hgreendg7@gmail.com>
2024-07-29 13:32:27 +0100

    Customisations

--- run_script.bat
+++ run_script.bat
@@ -1,3 +1,3 @@
 @echo off
-python "C:\Users\hjgre\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\File-Organiser.py"
+python "E:\Programming\Automatic-File-Organisation\File-Organiser.py"
 pause
